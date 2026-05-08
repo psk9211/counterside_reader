9ce1c566cd580dc3258b37ca6431b0e01bcfa31b
myrhhcaiah <myrhhcaiah@gmail.com>
2026-05-07 23:44:49 -0700

    Add cut-ins to viewer; Add MTL for untranslated CounterCases

---
+++ dist/assets/COUNTERCASE_CLOUDIA_NORMAL_1-rFvtWMBQ.js
@@ -0,0 +1,37 @@
+var e=[{id:`L1_1`,type:`subtitle`,speaker:null,text:{KOREA:`AEGIS 8 PBLIT SECURITY SEQUENCE: <color=#ffd34c>ACTIVATED</color>
+ADC FACTOR: <color=#ffd34c>BETA CONFIRMED</color>
+CN No. VCDF002S07056-10: <color=#ffd34c>Claudia Nelson</color>`,JPN:`AEGIS 8 PBLIT SECURITY SEQUENCE: <color=#ffd34c>ACTIVATED</color>
+ADC FACTOR: <color=#ffd34c>BETA CONFIRMED</color>
+CN No. VCDF002S07056-10: <color=#ffd34c>Claudia Nelson</color>`,ENG:`AEGIS 8 PBLIT SECURITY SEQUENCE: <color=#ffd34c>ACTIVATED</color>
+ADC FACTOR: <color=#ffd34c>BETA CONFIRMED</color>
+CN No. VCDF002S07056-10: <color=#ffd34c>Claudia Nelson</color>`,TWN:`(TWN)AEGIS 8 PBLIT SECURITY SEQUENCE: <color=#ffd34c>ACTIVATED</color>
+ADC FACTOR: <color=#ffd34c>BETA CONFIRMED</color>
+CN No. VCDF002S07056-10: <color=#ffd34c>Claudia Nelson</color>`},stage:{bg:`BLACK`,bgm:`CUTSCENE_CONSPIRACY`,filter:`NCFT_NORMAL`,slots:{L:null,C:null,R:null}}},{id:`L3_1`,type:`dialog`,speaker:null,text:{KOREA:`관리국 부여 코드네임: 트루 크로스
+<color=#ffd34c>Registrated by the Administrator</color>`,JPN:`管理局付与コードネーム: トゥルークロス
+<color=#ffd34c>Registrated by the Administrator</color>`,ENG:`Administrative Code Name: True Cross
+<color=#ffd34c>Registrated by the Administrator</color>`,TWN:`(TWN)관리국 부여 코드네임: 트루 크로스
+<color=#ffd34c>Registrated by the Administrator</color>`},stage:{bg:`BLACK`,bgm:`CUTSCENE_CONSPIRACY`,filter:`NCFT_NORMAL`,slots:{L:null,C:{char_id:`CLOUDIA_VATICAN_NULL_NULL`,face:`UNIT_IDLE`,hologram:!1},R:null}}},{id:`L4_1`,type:`dialog`,speaker:null,text:{KOREA:`성명: 클로디아 넬슨
+소속: 신앙교리성 부설 3과`,JPN:`声明: クラウディア・ネルソン
+所属: 信仰教理性敷設3科`,ENG:`Name: Claudia Nelson
+Affiliation: Congregation for the Doctrine of the Faith, Sub-Department 3`,TWN:`(TWN)성명: 클로디아 넬슨
+소속: 신앙교리성 부설 3과`},stage:{bg:`BLACK`,bgm:`CUTSCENE_CONSPIRACY`,filter:`NCFT_NORMAL`,slots:{L:null,C:{char_id:`CLOUDIA_VATICAN_NULL_NULL`,face:`UNIT_IDLE`,hologram:!1},R:null}}},{id:`L5_1`,type:`dialog`,speaker:null,text:{KOREA:`분류: Counter
+등급: <color=#ff3636>B+</color>             `,JPN:`分類: Counter
+等級: <color=#ff3636>B+</color>`,ENG:`Classification: Counter
+Rank: <color=#ff3636>B+</color>`,TWN:`(TWN)분류: Counter
+등급: <color=#ff3636>B+</color>             `},stage:{bg:`BLACK`,bgm:`CUTSCENE_CONSPIRACY`,filter:`NCFT_NORMAL`,slots:{L:null,C:{char_id:`CLOUDIA_VATICAN_NULL_NULL`,face:`UNIT_IDLE`,hologram:!1},R:null}}},{id:`L6_1`,type:`dialog`,speaker:null,text:{KOREA:`신체 연령: 만 18세
+각성 시점: 2019.12.24             `,JPN:`身体年齢: 満18歳
+覚醒時点: 2019.12.24`,ENG:`Physical Age: 18
+Awakening Date: 2019.12.24`,TWN:`(TWN)신체 연령: 만 18세
+각성 시점: 2019.12.24             `},stage:{bg:`BLACK`,bgm:`CUTSCENE_CONSPIRACY`,filter:`NCFT_NORMAL`,slots:{L:null,C:{char_id:`CLOUDIA_VATICAN_NULL_NULL`,face:`UNIT_IDLE`,hologram:!1},R:null}}},{id:`L7_1`,type:`dialog`,speaker:null,text:{KOREA:`성별: 여성
+출신: 웨일스`,JPN:`性別: 女性
+出身: ウェールズ`,ENG:`Gender: Female
+Origin: Wales`,TWN:`(TWN)성별: 여성
+출신: 웨일스`},stage:{bg:`BLACK`,bgm:`CUTSCENE_CONSPIRACY`,filter:`NCFT_NORMAL`,slots:{L:null,C:{char_id:`CLOUDIA_VATICAN_NULL_NULL`,face:`UNIT_IDLE`,hologram:!1},R:null}}},{id:`L8_1`,type:`dialog`,speaker:null,text:{KOREA:`신장: 5' 4.1'' / 165cm
+체중: 50kg                   `,JPN:`伸張: 5'4.1'' / 165cm
+体重: 50kg`,ENG:`Height: 5' 4.1'' / 165cm
+Weight: 50kg`,TWN:`(TWN)신장: 5' 4.1'' / 165cm
+체중: 50kg                   `},stage:{bg:`BLACK`,bgm:`CUTSCENE_CONSPIRACY`,filter:`NCFT_NORMAL`,slots:{L:null,C:{char_id:`CLOUDIA_VATICAN_NULL_NULL`,face:`UNIT_IDLE`,hologram:!1},R:null}}},{id:`L9_1`,type:`dialog`,speaker:null,text:{KOREA:`모발 색상: 은백색
+홍채 색상: 비취색`,JPN:`毛髪色: 銀白色
+虹彩色: 矢翠色`,ENG:`Hair Color: Silver-white
+Iris Color: Jade`,TWN:`(TWN)모발 색상: 은백색
+홍채 색상: 비취색`},stage:{bg:`BLACK`,bgm:`CUTSCENE_CONSPIRACY`,filter:`NCFT_NORMAL`,slots:{L:null,C:{char_id:`CLOUDIA_VATICAN_NULL_NULL`,face:`UNIT_IDLE`,hologram:!1},R:null}}},{id:`L10_1`,type:`dialog`,speaker:null,text:{KOREA:`해당 인물의 핵심 자료 공개를 청구함 - Leo Bardem, MD`,JPN:`該当の人物の核心資料公開を請求する - Leo Bardem、MD`,ENG:`Request for disclosure of core data regarding this subject - Leo Bardem, MD`,TWN:`(TWN)해당 인물의 핵심 자료 공개를 청구함 - Leo Bardem, MD`},stage:{bg:`BLACK`,bgm:`CUTSCENE_CONSPIRACY`,filter:`NCFT_NORMAL`,slots:{L:null,C:{char_id:`CLOUDIA_VATICAN_NULL_NULL`,face:`UNIT_IDLE`,hologram:!1},R:null}}},{id:`L11_1`,type:`dialog`,speaker:null,text:{KOREA:`해당 인물의 핵심 자료 공개 청구는 소속 기관의 기밀 보호 정책에 의거하여 거부됨 - APO`,JPN:`該当の人物の核心資料公開請求は所属機関の機密保護政策に基づいて拒否される - APO`,ENG:`Request for disclosure of core data regarding this subject denied pursuant to confidential protection policy of affiliated institution - APO`,TWN:`(TWN)해당 인물의 핵심 자료 공개 청구는 소속 기관의 기밀 보호 정책에 의거하여 거부됨 - APO`},stage:{bg:`BLACK`,bgm:`CUTSCENE_CONSPIRACY`,filter:`NCFT_NORMAL`,slots:{L:null,C:{char_id:`CLOUDIA_VATICAN_NULL_NULL`,face:`UNIT_IDLE`,hologram:!1},R:null}}}];export{e as default};
---
+++ dist/assets/COUNTERCASE_LIV_NORMAL_1-MDySnc0l.js
@@ -0,0 +1,33 @@
+var e=[{id:`L1_1`,type:`subtitle`,speaker:null,text:{KOREA:`NETBLINK SECURITY SEQUENCE: <color=#ffd34c>ACTIVATED</color>
+IDENTIFICATION: <color=#ffd34c>CONFIRMED</color>
+CCN No. CSM002M000537-49: <color=#ffd34c>에블린 워커</color>`,JPN:`NETBLINK SECURITY SEQUENCE: <color=#ffd34c>ACTIVATED</color>
+IDENTIFICATION: <color=#ffd34c>CONFIRMED</color>
+CCN No. CSM002M000537-49: <color=#ffd34c>イーヴリンウォーカー</color>`,ENG:`NETBLINK SECURITY SEQUENCE: <color=#ffd34c>ACTIVATED</color>
+IDENTIFICATION: <color=#ffd34c>CONFIRMED</color>
+CCN No. CSM002M000537-49: <color=#ffd34c>Evelyn Walker</color>`,TWN:`(TWN)NETBLINK SECURITY SEQUENCE: <color=#ffd34c>ACTIVATED</color>
+IDENTIFICATION: <color=#ffd34c>CONFIRMED</color>
+CCN No. CSM002M000537-49: <color=#ffd34c>에블린 워커</color>`},stage:{bg:`BLACK`,bgm:null,filter:`NCFT_NORMAL`,slots:{L:null,C:null,R:null}}},{id:`L4_1`,type:`dialog`,speaker:null,text:{KOREA:`성명: 리브 앨런`,JPN:`声明: リブ・アレン`,ENG:`Name: Liv Allen`,TWN:`(TWN)성명: 리브 앨런`},stage:{bg:`BLACK`,bgm:null,filter:`NCFT_NORMAL`,slots:{L:null,C:{char_id:`LIV_ZODIAC_NULL_NULL`,face:`UNIT_IDLE`,hologram:!1},R:null}}},{id:`L5_1`,type:`dialog`,speaker:null,text:{KOREA:`분류: 카운터
+등급: D`,JPN:`分類: カウンター
+等級: D`,ENG:`Classification: Counter
+Rank: D`,TWN:`(TWN)분류: 카운터
+등급: D`},stage:{bg:`BLACK`,bgm:null,filter:`NCFT_NORMAL`,slots:{L:null,C:{char_id:`LIV_ZODIAC_NULL_NULL`,face:`UNIT_IDLE`,hologram:!1},R:null}}},{id:`L6_1`,type:`dialog`,speaker:null,text:{KOREA:`신장: 155cm
+체중: 42.3kg`,JPN:`伸張: 155cm
+体重: 42.3kg`,ENG:`Height: 155cm
+Weight: 42.3kg`,TWN:`(TWN)신장: 155cm
+체중: 42.3kg`},stage:{bg:`BLACK`,bgm:null,filter:`NCFT_NORMAL`,slots:{L:null,C:{char_id:`LIV_ZODIAC_NULL_NULL`,face:`UNIT_IDLE`,hologram:!1},R:null}}},{id:`L7_1`,type:`dialog`,speaker:null,text:{KOREA:`소속: 조디악 나이츠
+계급: 천칭궁사`,JPN:`所属: チョディアックナイツ
+階級: 天秤宮社`,ENG:`Affiliation: Zodiac Knights
+Rank: Libra Paladin`,TWN:`(TWN)소속: 조디악 나이츠
+계급: 천칭궁사`},stage:{bg:`BLACK`,bgm:null,filter:`NCFT_NORMAL`,slots:{L:null,C:{char_id:`LIV_ZODIAC_NULL_NULL`,face:`UNIT_IDLE`,hologram:!1},R:null}}},{id:`L8_1`,type:`dialog`,speaker:null,text:{KOREA:`무장: 주베넬 스태프
+특기: 가사 전반`,JPN:`武装: チュベネルスタッフ
+特技: 家事全般`,ENG:`Armament: Juvenal Staff
+Specialty: General Housework`,TWN:`(TWN)무장: 주베넬 스태프
+특기: 가사 전반`},stage:{bg:`BLACK`,bgm:null,filter:`NCFT_NORMAL`,slots:{L:null,C:{char_id:`LIV_ZODIAC_NULL_NULL`,face:`UNIT_IDLE`,hologram:!1},R:null}}},{id:`L9_1`,type:`dialog`,speaker:null,text:{KOREA:`좋아하는 것: 치즈버거
+싫어하는 것: 잔소리`,JPN:`好きなの: チーズバーガー
+嫌がるの: 小言`,ENG:`Likes: Cheeseburgers
+Dislikes: Nagging`,TWN:`(TWN)좋아하는 것: 치즈버거
+싫어하는 것: 잔소리`},stage:{bg:`BLACK`,bgm:null,filter:`NCFT_NORMAL`,slots:{L:null,C:{char_id:`LIV_ZODIAC_NULL_NULL`,face:`UNIT_IDLE`,hologram:!1},R:null}}},{id:`L10_1`,type:`dialog`,speaker:null,text:{KOREA:`항상 앞장서서 적진으로 뛰어드는 조디악 나이츠의 사고뭉치.
+지휘관인 피오네와 하루가 멀다 하고 티격태격한다.`,JPN:`常に先頭に立って敵陣で跳び込むチョディアックナイツのわっぱ。
+指揮官であるピオネと一日が遠いしてなんだかんだと言う。`,ENG:`The Zodiac Knights' troublemaker who always charges headfirst into enemy lines.
+Constantly bickers with her commander, Pione.`,TWN:`(TWN)항상 앞장서서 적진으로 뛰어드는 조디악 나이츠의 사고뭉치.
+지휘관인 피오네와 하루가 멀다 하고 티격태격한다.`},stage:{bg:`BLACK`,bgm:null,filter:`NCFT_NORMAL`,slots:{L:null,C:{char_id:`LIV_ZODIAC_NULL_NULL`,face:`UNIT_IDLE`,hologram:!1},R:null}}}];export{e as default};
